2e0547412cfd418cbef510f09e200e9c1a8a73a4
Oliver Voogd <51314988+OliverVoogd@users.noreply.github.com>
2025-01-28 14:16:22 +1100

    Day Two

---
+++ 2/main1.cpp
@@ -0,0 +1,81 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+typedef std::vector<int> Report;
+
+int sign(int i)
+{
+	if (i == 0)
+		return 0;
+	if (i < 0)
+		return -1;
+	return 1;
+}
+
+Report splitStringToReport(const std::string &line)
+{
+	std::string tmp;
+	std::stringstream ss(line);
+	Report report;
+	while (std::getline(ss, tmp, ' '))
+	{
+		report.push_back(std::stoi(tmp));
+	}
+
+	return report;
+}
+
+std::vector<Report> parseReports(const std::string &input)
+{
+	std::ifstream file(input);
+	std::string line;
+	std::vector<Report> reports;
+
+	while (std::getline(file, line))
+	{
+		reports.push_back(splitStringToReport(line));
+	}
+
+	file.close();
+	return reports;
+}
+
+bool isReportSafe(const Report &report)
+{
+	int gradient = 0;
+	for (int i = 0; i < report.size() - 1; i++)
+	{
+		int thisDiff = report[i + 1] - report[i];
+		if (!gradient)
+		{
+			gradient = sign(thisDiff);
+		}
+		else
+		{
+			if (sign(thisDiff) != gradient)
+				return false;
+		}
+
+		if (std::abs(thisDiff) < 1 || std::abs(thisDiff) > 3)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	// std::string input = "test.txt";
+	std::string input = "input.txt";
+	std::vector<Report> reports = parseReports(input);
+
+	int numSafe = 0;
+	for (auto r : reports)
+	{
+		numSafe += int(isReportSafe(r));
+	}
+
+	std::cout << "Number of Safe Reports: " << numSafe << std::endl;
+}
